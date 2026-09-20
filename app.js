218d20b0af28c8ed40c69f9b98c23c371ac07c6a
sonu <ahemadsiddiqui786@gmail.com>
2026-09-20 11:38:14 +0530

    updates

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const morgan = require('morgan');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 const app = express();
+
+
 // 1) MIDDLEWARES
 if (process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'));
@@ -13,10 +15,6 @@ app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
 app.use((req,res,next) => {
-    console.log('Hello from the middleware 👋');
-    next();
-});
-app.use((req,res,next) => {
     req.requestTime = new Date().toISOString();
     next();
 })
@@ -24,5 +22,12 @@ app.use((req,res,next) => {
 // 3) Routes
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+
+// app.all('*', (req, res, next) => {
+//     res.status(404).json({
+//         status: 'fail',
+//         message: `Can't find ${req.originalUrl} on this server!`
+//     });
+// });
 
 module.exports = app;
